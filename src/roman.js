9d71d70e299bf4916a3e51be1a0d89862f1315aa
ScottNC <133363623+ScottNC@users.noreply.github.com>
2023-06-03 18:47:34 +0100

    removes unecessary optional chaining

--- src/roman.js
+++ src/roman.js
@@ -13,13 +13,13 @@ export function numToRoman(num) {
         num -= power * x;
 
         if (x === 4) 
-            roman += ROMAN_NUMERALS[power]?.[1] + ROMAN_NUMERALS[power]?.[5];
+            roman += ROMAN_NUMERALS[power][1] + ROMAN_NUMERALS[power][5];
         else if (x === 9)
-            roman += ROMAN_NUMERALS[power]?.[1] + ROMAN_NUMERALS[power * 10]?.[1];
+            roman += ROMAN_NUMERALS[power][1] + ROMAN_NUMERALS[power * 10][1];
         else if (x >= 5)
-            roman += ROMAN_NUMERALS[power]?.[5] + ROMAN_NUMERALS[power]?.[1].repeat(x - 5);
+            roman += ROMAN_NUMERALS[power][5] + ROMAN_NUMERALS[power][1].repeat(x - 5);
         else if (x > 0)
-            roman += ROMAN_NUMERALS[power]?.[1].repeat(x);
+            roman += ROMAN_NUMERALS[power][1].repeat(x);
 
         return roman;
     }, '');
